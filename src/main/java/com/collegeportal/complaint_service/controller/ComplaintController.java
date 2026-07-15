28af2361a1280a13594d27a8fe1df0ff8148f8cf
Shivam314115 <choureyshivam281@gmail.com>
2026-07-16 01:57:51 +0530

    fix: remove local CORS to let API Gateway handle it

--- src/main/java/com/collegeportal/complaint_service/controller/ComplaintController.java
+++ src/main/java/com/collegeportal/complaint_service/controller/ComplaintController.java
@@ -17,7 +17,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/complaints")
 @RequiredArgsConstructor
-@CrossOrigin("*")
 public class ComplaintController {
 
     private final ComplaintService complaintService;
